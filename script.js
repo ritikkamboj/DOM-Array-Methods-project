a9da66fb79b02f850c5968bc702ba587295f56a0
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-08-11 17:12:56 +0530

    used forEach and manipulated DOM dynamically

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
-const main = document.getElementById('main');
+const main = document.getElementById("main");
 
-const addUser = document.getElementById('add-user');
-const double = document.getElementById('double');
-const showMill = document.getElementById('show-millionaires');
-const sort = document.getElementById('sort');
-const calWealth = document.getElementById('calculate-wealth');
+const addUser = document.getElementById("add-user");
+const double = document.getElementById("double");
+const showMill = document.getElementById("show-millionaires");
+const sort = document.getElementById("sort");
+const calWealth = document.getElementById("calculate-wealth");
 
-
-const data =[];
+const data = [];
 
 getRandomUser();
 getRandomUser();
 getRandomUser();
 
-async function getRandomUser()
-{
-    const res = await fetch(`https://randomuser.me/api/`);
+async function getRandomUser() {
+  const res = await fetch(`https://randomuser.me/api/`);
 
-    const data = await res.json();
-    // console.log(data.results[0]);
+  const data = await res.json();
+  // console.log(data.results[0]);
 
-    let randomUser = {
-        name : `${data.results[0].name.first} ${data.results[0].name.last}`,
-        income : Math.floor(Math.random()*1000000),
-
-    }
+  let randomUser = {
+    name: `${data.results[0].name.first} ${data.results[0].name.last}`,
+    income: Math.floor(Math.random() * 1000000),
+  };
 
   addData(randomUser);
 }
 
-function addData(obj)
-{
-    data.push(obj);
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
 }
 
+function formatAmount(value)
+{
+    return `₹` +(value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');  // 12,345.67
+}
 
+function updateDOM(storedData = data) {
+  main.innerHTML = ` <h2><strong>Person</strong>Wealth</h2>`;
+
+  storedData.forEach(item=>{
+    const element = document.createElement('div');
+    element.classList.add('person');
+    console.log(element)
+    element.innerHTML = `<strong>${item.name}</strong>${formatAmount(item.income)}`;
+    main.appendChild(element);
+  })
+}
+
+addUser.addEventListener('click',getRandomUser);
 
 console.log(data);
-
